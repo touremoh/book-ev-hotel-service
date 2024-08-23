5e94d033768af0fffd907ade1b7680e0ceb49745
Mohamed Toure <demahom.toure@gmail.com>
2024-08-23 14:13:10 +0200

    First commit

---
+++ src/main/java/com/bookevhotel/bookevhotelservice/BookEvHotelServiceApplication.java
@@ -0,0 +1,14 @@
+package com.bookevhotel.bookevhotelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class BookEvHotelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookEvHotelServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookevhotel/bookevhotelservice/BookEvHotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookevhotel.bookevhotelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEvHotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
