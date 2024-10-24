4979afb7efa8623fea2e258eda1f120799314a40
Mohamed Toure <demahom.toure@gmail.com>
2024-10-24 15:42:08 +0200

    Remove languageCode from response

--- src/main/java/com/bookevhotel/core/dto/AbstractBookEVHotelDTO.java
+++ src/main/java/com/bookevhotel/core/dto/AbstractBookEVHotelDTO.java
@@ -1,5 +1,6 @@
 package com.bookevhotel.core.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -8,5 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 @Getter
 @Setter
 public abstract class AbstractBookEVHotelDTO implements BookEVHotelDTO {
-		protected String languageCode;
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	protected String languageCode;
 }
